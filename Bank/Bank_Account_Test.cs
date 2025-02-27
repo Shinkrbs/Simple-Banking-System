2852f7c5f51965a4275472e4e4a9c8b63632b635
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-02-27 20:25:40 +0800

    Update on Bank Account Unit Test
    Finished all necessary Bank Account Class unit testing.

--- Bank/Bank_Account_Test.cs
+++ Bank/Bank_Account_Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Simple_Banking_System.Classes;
 
@@ -12,6 +13,7 @@ namespace Test
         {
             Bank_Account b = new Bank_Account();
             var ex = Assert.ThrowsException<NegativeAmountException>(() => b.Deposit(-100.0));
+            Assert.AreEqual("Amount cannot be negative", ex.Message);
         }
 
         [TestMethod]
@@ -28,6 +30,31 @@ namespace Test
             Bank_Account b = new Bank_Account();
             b.Deposit(100.0);
             Assert.AreEqual(100.0, b.GetBalance());
+        }
+
+        [TestMethod]
+        public void Withdraw_DecreaseAmount()
+        {
+            Bank_Account b = new Bank_Account();
+            b.Deposit(100.0);
+            b.Withdraw(50.0);
+            Assert.AreEqual(50.0, b.GetBalance());
+        }
+
+        [TestMethod]
+        public void Withdraw_NegativeAmount()
+        {
+            Bank_Account b = new Bank_Account();
+            var ex = Assert.ThrowsException<NegativeAmountException>(() => b.Withdraw(-100.0));
+            Assert.AreEqual("Amount cannot be negative", ex.Message);
+        }
+
+        [TestMethod]
+        public void Withdraw_InsuficcientFunds()
+        {
+            Bank_Account b = new Bank_Account();
+            var ex = Assert.ThrowsException<InsufficientFundsException>(() => b.Withdraw(0.0));
+            Assert.AreEqual("Amount is greater than available balance", ex.Message);
         }
     }
 }

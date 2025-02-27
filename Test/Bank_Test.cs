d0f5d6e527bf6c7aeda1c163b6f7d8c865a905fe
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-02-28 00:56:32 +0800

    Added the Test File

---
+++ Test/Bank_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simple_Banking_System.Classes;
+using Simple_Banking_System.Enums;
+
+namespace Test
+{
+	[TestClass]
+	public class Bank_Test
+	{
+		private Bank b;
+
+	}
+}

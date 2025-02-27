9038023e0fbff6607a77bf6363662b8e197ba253
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-02-27 23:24:46 +0800

    Update on Bank Account Class Unit Testing
    Made Bank Account Code Readability much better for unit testing.

--- Bank/Bank_Account_Test.cs
+++ Bank/Bank_Account_Test.cs
@@ -9,20 +9,29 @@ namespace Test
     [TestClass]
     public class Bank_Account_Test
     {
+        #region Setup
+        private Bank_Account b;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            b = new Bank_Account("123459987", 100.0);
+        }
+        #endregion
+
         #region Constructor Tests
         [TestMethod]
         public void Constructor_Default()
         {
-            Bank_Account b = new Bank_Account();
-            Assert.AreEqual("0000000000", b.AccountNumber);
-            Assert.AreEqual(0.0, b.Balance);
-            Assert.AreEqual(Account_Type.Savings, b.AccountType);
+            Bank_Account dummy = new Bank_Account();
+            Assert.AreEqual("0000000000", dummy.AccountNumber);
+            Assert.AreEqual(0.0, dummy.Balance);
+            Assert.AreEqual(Account_Type.Savings, dummy.AccountType);
         }
 
         [TestMethod]
         public void Constructor_Mandatory()
         {
-            Bank_Account b = new Bank_Account("123459987", 123.00);
             Assert.AreEqual("123459987", b.AccountNumber);
             Assert.AreEqual(123.00, b.Balance);
         }
@@ -30,17 +39,16 @@ namespace Test
         public void Constructor_Mandatory_AllFields()
         {
             // Assuming Account Type is Business Type
-            Bank_Account b = new Bank_Account("123459987", 123.00, Account_Type.Business);
-            Assert.AreEqual("123459987", b.AccountNumber);
-            Assert.AreEqual(123.00, b.Balance);
-            Assert.AreEqual(Account_Type.Business, b.AccountType);
+            Bank_Account dummy = new Bank_Account("123459987", 123.00, Account_Type.Business);
+            Assert.AreEqual("123459987", dummy.AccountNumber);
+            Assert.AreEqual(123.00, dummy.Balance);
+            Assert.AreEqual(Account_Type.Business, dummy.AccountType);
         }
         #endregion
 
         [TestMethod]
         public void Deposit_Negative_throwException()
         {
-            Bank_Account b = new Bank_Account();
             var ex = Assert.ThrowsException<NegativeAmountException>(() => b.Deposit(-100.0));
             Assert.AreEqual("Amount cannot be negative", ex.Message);
         }
@@ -48,24 +56,19 @@ namespace Test
         [TestMethod]
         public void Deposit_IncreaseAmount()
         {
-            Bank_Account b = new Bank_Account();
             b.Deposit(100.0);
-            Assert.AreEqual(100.0, b.GetBalance());
+            Assert.AreEqual(200.0, b.GetBalance());
         }
 
         [TestMethod]
         public void GetBalance_ReturnsBalance()
         {
-            Bank_Account b = new Bank_Account();
-            b.Deposit(100.0);
             Assert.AreEqual(100.0, b.GetBalance());
         }
 
         [TestMethod]
         public void Withdraw_DecreaseAmount()
         {
-            Bank_Account b = new Bank_Account();
-            b.Deposit(100.0);
             b.Withdraw(50.0);
             Assert.AreEqual(50.0, b.GetBalance());
         }
@@ -73,7 +76,6 @@ namespace Test
         [TestMethod]
         public void Withdraw_NegativeAmount()
         {
-            Bank_Account b = new Bank_Account();
             var ex = Assert.ThrowsException<NegativeAmountException>(() => b.Withdraw(-100.0));
             Assert.AreEqual("Amount cannot be negative", ex.Message);
         }
@@ -81,7 +83,6 @@ namespace Test
         [TestMethod]
         public void Withdraw_InsuficcientFunds()
         {
-            Bank_Account b = new Bank_Account();
             var ex = Assert.ThrowsException<InsufficientFundsException>(() => b.Withdraw(10.0));
             Assert.AreEqual("Amount is greater than available balance", ex.Message);
         }

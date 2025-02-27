4149cce8c555a8f0f1bbc1198c82d1cc5e055e4c
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-02-27 22:00:20 +0800

    Update on Customer Class Unit Testing
    Made Setup Data and Setup Test Function. Also added Constructor unit tests.

--- Bank/Customer_Test.cs
+++ Bank/Customer_Test.cs
@@ -1,11 +1,66 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simple_Banking_System.Classes;
+using Simple_Banking_System.Enums;
 
 namespace Bank
 {
 	[TestClass]
 	public class Customer_Test
 	{
-	
-	}
+        #region Test Setup
+        private Customer c;
+        private DateTime Fixed_Account_CreationTime;
+        private string cID;
+        private string fName;
+        private string lName;
+        private string Fixed_Email;
+        private string mName;
+        private string address;
+        private int age;
+        
+
+        [TestInitialize]
+        public void Setup()
+        {
+            Fixed_Account_CreationTime = new DateTime(2025, 2, 27, 10, 0, 0); // Feb. 27, 2025 !0:00
+            c = new Customer("123456", "John", "Doe", Fixed_Account_CreationTime);
+
+            cID = c.CustomerID;
+            fName = c.FirstName;
+            lName = c.LastName;
+            Fixed_Email = "example@gmail.com";
+            mName = "Cruz";
+            address = "1234 Artikulo Burgos St.";
+            age = 21;
+        }
+        #endregion
+
+        #region Constructor Tests
+
+        [TestMethod]
+        public void Constructor_Mandatory()
+        {
+            Assert.AreEqual(cID, c.CustomerID);
+            Assert.AreEqual(fName, c.FirstName);
+            Assert.AreEqual(lName, c.LastName);
+            Assert.AreEqual("", c.Email);
+            Assert.AreEqual(Fixed_Account_CreationTime, c.AccountCreationDate);
+        }
+
+        [TestMethod]
+        public void Constructor_Optional()
+        {
+            c = new Customer(cID, fName, lName, Fixed_Account_CreationTime, mName, address, age);
+            Assert.AreEqual(cID, c.CustomerID);
+            Assert.AreEqual(fName, c.FirstName);
+            Assert.AreEqual(lName, c.LastName);
+            Assert.AreEqual("", c.Email);
+            Assert.AreEqual(Fixed_Account_CreationTime, c.AccountCreationDate);
+            Assert.AreEqual(mName, c.MiddleName);
+            Assert.AreEqual(address, c.Address);
+            Assert.AreEqual(age, c.Age);
+        }
+        #endregion
+    }
 }

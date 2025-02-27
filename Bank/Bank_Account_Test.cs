5384dde7f2f83821b73f73836bc1c57e1bbedab1
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-02-27 21:27:29 +0800

    Update on Bank Account Unit Test and Added new Customer Class Unit Test
    Added Unit Test for Constructors, and started to
    add Customer Class Test.

--- Bank/Bank_Account_Test.cs
+++ Bank/Bank_Account_Test.cs
@@ -2,12 +2,41 @@
 using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Simple_Banking_System.Classes;
+using Simple_Banking_System.Enums;
 
 namespace Test
 {
     [TestClass]
     public class Bank_Account_Test
     {
+        #region Constructor Tests
+        [TestMethod]
+        public void Constructor_Default()
+        {
+            Bank_Account b = new Bank_Account();
+            Assert.AreEqual("0000000000", b.AccountNumber);
+            Assert.AreEqual(0.0, b.Balance);
+            Assert.AreEqual(Account_Type.Savings, b.AccountType);
+        }
+
+        [TestMethod]
+        public void Constructor_Mandatory()
+        {
+            Bank_Account b = new Bank_Account("123459987", 123.00);
+            Assert.AreEqual("123459987", b.AccountNumber);
+            Assert.AreEqual(123.00, b.Balance);
+        }
+        [TestMethod]
+        public void Constructor_Mandatory_AllFields()
+        {
+            // Assuming Account Type is Business Type
+            Bank_Account b = new Bank_Account("123459987", 123.00, Account_Type.Business);
+            Assert.AreEqual("123459987", b.AccountNumber);
+            Assert.AreEqual(123.00, b.Balance);
+            Assert.AreEqual(Account_Type.Business, b.AccountType);
+        }
+        #endregion
+
         [TestMethod]
         public void Deposit_Negative_throwException()
         {
---
+++ Bank/Customer_Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bank
+{
+	[TestClass]
+	public class Customer_Test
+	{
+	
+	}
+}
